4cce2abeb6594c956f0ff83a4e45ab5130a18cfc
Himanshu Singh <himanshu.kumar.singh@gmail.com>
2024-11-24 18:41:53 +0530

    increase accuracy

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import torch
 
 RANDOM_SEED = 42
 BATCH_SIZE = 64
-EPOCHS = 18
+EPOCHS = 19
 LEARNING_RATE = 0.01
 MOMENTUM = 0.9
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
