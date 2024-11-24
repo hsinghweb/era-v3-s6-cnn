d51e4ae4adb712b7ebbce0953111b11327849d1c
Himanshu Singh <himanshu.kumar.singh@gmail.com>
2024-11-24 20:43:45 +0530

    tune model configs

--- src/model.py
+++ src/model.py
@@ -6,37 +6,35 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # First Block
-        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)  # Changed output to 16
-        self.bn1 = nn.BatchNorm2d(16)
+        self.conv1 = nn.Conv2d(1, 8, 3, padding=1)  # Reduced to 8 channels
+        self.bn1 = nn.BatchNorm2d(8)
         
         # Second Block
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)  # Changed input to 16 to match previous layer
-        self.bn2 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(8, 16, 3, padding=1)  # Reduced to 16 channels
+        self.bn2 = nn.BatchNorm2d(16)
         self.pool1 = nn.MaxPool2d(2, 2)
-        self.dropout1 = nn.Dropout(0.05)
+        self.dropout1 = nn.Dropout(0.1)  # Increased dropout
         
         # Third Block
-        self.conv3 = nn.Conv2d(32, 32, 3, padding=1)  # Changed input to 32 to match previous layer
-        self.bn3 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(16, 20, 3, padding=1)  # Reduced to 20 channels
+        self.bn3 = nn.BatchNorm2d(20)
         self.pool2 = nn.MaxPool2d(2, 2)
-        self.dropout2 = nn.Dropout(0.05)
+        self.dropout2 = nn.Dropout(0.1)  # Increased dropout
         
         # Fourth Block with parallel paths
-        self.conv4_1x1 = nn.Conv2d(32, 32, 1)  # Changed input and output to 32
-        self.conv4_main = nn.Conv2d(32, 32, 3, padding=1)  # Changed input to 32
-        self.bn4 = nn.BatchNorm2d(32)
+        self.conv4_1x1 = nn.Conv2d(20, 20, 1)  # Reduced to 20 channels
+        self.conv4_main = nn.Conv2d(20, 20, 3, padding=1)  # Reduced to 20 channels
+        self.bn4 = nn.BatchNorm2d(20)
         
-        # Fifth Block with squeeze-excitation
-        self.conv5 = nn.Conv2d(32, 32, 3, padding=1)  # Changed input and output to 32
-        self.bn5 = nn.BatchNorm2d(32)
-        self.se_fc1 = nn.Linear(32, 24)  # squeeze
-        self.se_fc2 = nn.Linear(24, 32)  # excitation
+        # Fifth Block
+        self.conv5 = nn.Conv2d(20, 20, 3, padding=1)  # Reduced to 20 channels
+        self.bn5 = nn.BatchNorm2d(20)
         
         # Global Average Pooling
         self.gap = nn.AdaptiveAvgPool2d(1)
         
-        # Final FC Layers
-        self.fc = nn.Linear(32, 10)  # Changed input to 32
+        # Final FC Layer
+        self.fc = nn.Linear(20, 10)
 
     def forward(self, x):
         # First Block
@@ -44,27 +42,24 @@ class Net(nn.Module):
         
         # Second Block
         x = F.relu(self.bn2(self.conv2(x)))
-        x = self.dropout1(self.pool1(x))
+        x = self.pool1(x)
+        x = self.dropout1(x)
         
         # Third Block
         x = F.relu(self.bn3(self.conv3(x)))
-        x = self.dropout2(self.pool2(x))
+        x = self.pool2(x)
+        x = self.dropout2(x)
         
-        # Fourth Block with parallel paths
-        x_1x1 = self.conv4_1x1(x)
+        # Fourth Block with skip connection
         x_main = F.relu(self.bn4(self.conv4_main(x)))
-        x = x_main + F.relu(x_1x1)  # residual-like connection
+        x_1x1 = self.conv4_1x1(x)
+        x = x_main + F.relu(x_1x1)
         
-        # Fifth Block with squeeze-excitation
-        x_se = self.gap(x)
-        x_se = x_se.view(-1, 32)
-        x_se = F.relu(self.se_fc1(x_se))
-        x_se = torch.sigmoid(self.se_fc2(x_se))
-        x_se = x_se.view(-1, 32, 1, 1)
-        x = F.relu(self.bn5(self.conv5(x))) * x_se
+        # Fifth Block
+        x = F.relu(self.bn5(self.conv5(x)))
         
-        # GAP and FC
+        # Global Average Pooling and Final FC
         x = self.gap(x)
-        x = x.view(-1, 32)
-        x = F.relu(self.fc(x))
+        x = x.view(-1, 20)
+        x = self.fc(x)
         return F.log_softmax(x, dim=1) 
